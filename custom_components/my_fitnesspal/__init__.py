557fd24c829cbd09a6f0a80ccec985442b0bfb52
Damien Kingsley <actuallydamo@gmail.com>
2022-04-03 18:19:13 +1000

    Add support for kilojoules
    When attempting to setup an account with kilojoules set for energy,
    a ZeroDivisionError would be generated.
    
    This change adds support for accounts with kilojoules set for energy.
    Additionally this change provides both energy units for all attributes.
    
    Resolves helto4real/custom_component_myfitnesspal#20

--- custom_components/my_fitnesspal/__init__.py
+++ custom_components/my_fitnesspal/__init__.py
@@ -141,14 +141,24 @@ class MyFitnessPalDataUpdateCoordinator(DataUpdateCoordinator):
             if len(latest_record) > 1:
                 latest_weight = latest_record[1]
 
-        goal_calories = info.goals.get("calories", 0)
+        goal_calories = info.goals.get("calories",
+            round(info.goals.get("kilojoules", 0)  / 4.184)
+        )
+        goal_kilojoules = info.goals.get("kilojoules",
+            round(info.goals.get("calories", 0) * 4.184)
+        )
         goal_carbohydrates = info.goals.get("carbohydrates", 0)
         goal_fat = info.goals.get("fat", 0)
         goal_sodium = info.goals.get("sodium", 0)
         goal_sugar = info.goals.get("sugar", 0)
         goal_protein = info.goals.get("protein", 0)
 
-        total_calories = info.totals.get("calories", 0)
+        total_calories = info.totals.get("calories",
+            round(info.totals.get("kilojoules", 0)  / 4.184)
+        )
+        total_kilojoules = info.totals.get("kilojoules",
+            round(info.totals.get("calories", 0) * 4.184)
+        )
         total_carbohydrates = info.totals.get("carbohydrates", 0)
         total_fat = info.totals.get("fat", 0)
         total_sodium = info.totals.get("sodium", 0)
@@ -158,12 +168,19 @@ class MyFitnessPalDataUpdateCoordinator(DataUpdateCoordinator):
         _, weight = list(weights.items())[0] if len(weights) > 0 else 0, 0
 
         cardio_calories_burned = 0
+        cardio_kilojoules_burned = 0
         for exercise in info.exercises[0]:
-            cardio_calories_burned += exercise["calories burned"]
+            cardio_calories_burned += exercise.totals.get("calories burned",
+                round(exercise.totals.get("kilojoules burned", 0)  / 4.184)
+            )
+            cardio_kilojoules_burned += exercise.totals.get("kilojoules burned",
+                round(exercise.totals.get("calories burned", 0) * 4.184)
+            )
 
         result = {}
 
         result["goal_calories"] = goal_calories
+        result["goal_kilojoules"] = goal_kilojoules
         result["goal_carbohydrates"] = goal_carbohydrates
         result["goal_fat"] = goal_fat
         result["goal_sodium"] = goal_sodium
@@ -171,22 +188,29 @@ class MyFitnessPalDataUpdateCoordinator(DataUpdateCoordinator):
         result["goal_protein"] = goal_protein
 
         result["total_calories"] = total_calories
+        result["total_kilojoules"] = total_kilojoules
         result["total_carbohydrates"] = total_carbohydrates
         result["total_fat"] = total_fat
         result["total_sodium"] = total_sodium
         result["total_sugar"] = total_sugar
         result["total_protein"] = total_protein
 
+        result["cardio_kilojoules_burned"] = cardio_kilojoules_burned
         result["cardio_calories_burned"] = cardio_calories_burned
         result["water"] = water
         result["weight"] = latest_weight
         result["cal_remaining"] = goal_calories - total_calories
+        result["kj_remaining"] = goal_kilojoules - total_kilojoules
         result["cal_remaining_ex_workout"] = (
             goal_calories - total_calories - cardio_calories_burned
         )
+        result["kj_remaining_ex_workout"] = (
+            goal_kilojoules - total_kilojoules - cardio_kilojoules_burned
+        )
         result["cal_goal"] = goal_calories - cardio_calories_burned
+        result["kj_goal"] = goal_kilojoules - cardio_kilojoules_burned
         result["goal_pct"] = round(
-            (total_calories / (goal_calories + cardio_calories_burned)) * 100,
+            (total_kilojoules / (goal_kilojoules + cardio_kilojoules_burned)) * 100,
             0,
         )
 
